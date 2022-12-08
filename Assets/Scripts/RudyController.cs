26ef77bf75f173218fb47f307d264648786d7588
Carvin Joseph <cjoseph25@aoiths.org>
2022-12-08 08:52:11 -0500

    part 7

--- Assets/Scripts/RudyController.cs
+++ Assets/Scripts/RudyController.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class RudyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 0.1f * horizontal;
         position.y = position.y + 0.1f * vertical;
         transform.position = position;
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth); 
     }
 }
