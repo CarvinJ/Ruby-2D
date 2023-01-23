aeb8389e7ec2c379ca5ba9d1ae94ce6685750b56
Carvin Joseph <cjoseph25@aoiths.org>
2023-01-23 17:06:20 -0500

    Part 9
    https://github.com/CarvinJ/Ruby-2D

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,19 +5,61 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
-
+    float timer;
+    int direction = 1;
+    
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
-        position.x = position.x + Time.deltaTime * speed;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+        
         rigidbody2D.MovePosition(position);
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RudyController player = other.gameObject.GetComponent<RudyController >();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
